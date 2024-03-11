4773e2927d4e33a568c73724bfa61b12460b52fd
RAJ ABHISHEK <130246210+RajAbhishek0721@users.noreply.github.com>
2024-03-11 22:20:19 +0000

    datatypes basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId =23643
+let accountEmail="raj@google.com"
+var accountPass="123"
+//accountCity="Noida" // not a good practice
+let accountState;
+
+//accountId=2 // not allowed
+
+accountEmail="abhi@f=gmail.com"
+accountPass="2343"
+
+
+// prefer not to use var, because isuue in block and functional scope
+
+console.table([accountId,accountEmail,accountPass,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+4) // we are using node js not browser
+
+console.log(3
+    +
+    4);// code redability should be high
+
+
+let name ="raj" // string
+let age =38 // number
+let isLoggedIn = false //boolean
+
+// null -> standalone value (empty value)
+
+// undefined -> value defined hi nhi h
+
+// symbol -> to find uniqueness in components
+
+
+// object 
+
+console.log(typeof "raj");
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
